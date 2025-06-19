64dd21c7b820bb4b5a197af424fa51b4ca21a5a2
adithya <adithyarana447@gmail.com>
2025-06-19 17:10:58 +0530

    done

--- api/index.js
+++ api/index.js
@@ -2,6 +2,12 @@
 import serverless from 'serverless-http';
 import app from '../server/app';
 
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Vercel!");
+});
+
 const handler = serverless(app);
 
 export default handler;
